0af0ffa79942565190020a14bfd1b339e018214a
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-05-30 18:54:10 -0700

    This is an additional push, concerning local storage. Still trying to figure out why correct or incorrect is spilling over to another

--- Assets/quiz-script.js
+++ Assets/quiz-script.js
@@ -1,90 +1,161 @@
 var timeDisplay = document.querySelector("#time-clock");
 var buttonListen = document.querySelector("#quiz-start");
 var questionDisplay = document.querySelector("#question");
+var incorrectDisplay = document.querySelector("#incorrectScore");
+var correctDisplay = document.querySelector("#correctScore");
 var answerA = document.querySelector("#choiceA");
 var answerB = document.querySelector("#choiceB");
 var answerC = document.querySelector("#choiceC");
+
 var questionARRAY = [ 0 , 1 , 2 , 3];
-var timeHost = 500;
-var correct = localStorage.getItem("correct");
+var timeHost = 60;
+
+var progress = 0;
+var correct = 0;
 var incorrect = 0;
 
-function correctUP() {
-    correct++;
-    localStorage.setItem("correct" , correct);
-    questionSelector();
-}
 
-function incorrectUP() {
-    incorrect++;
-    for (var i = 0; i < 5; i++) {
-        timeHost = timeHost - 5;
+function grading() {
+    if (correct == 1) {
+        correct == 0;
+        questionSelector();
+        return correct;
+    } else {
+        incorrect++;
+        timeHost-5;
+        questionSelector();
+        return timeHost;
     }
 }
 
+
+function correctUp() {
+    correct++;
+    progress++;
+    questionSelector();
+    
+    return;
+}
+
+function incorrectUp() {
+    incorrect++;
+    timeHost=timeHost-5;
+    
+    
+    return;
+}
 
 
 
 function clockDown() {
     
     var timer = setInterval(function() {
-    
+        incorrectDisplay.textContent = "Incorrect answers: " + incorrect;
+        correctDisplay.textContent = "Correct answers: " + correct;
+
         timeHost--;
    
         timeDisplay.textContent = timeHost + "seconds";
         console.log( timeHost );
              
-        if (timeHost <= 0) {
+        if (timeHost <= 0 || progress == 4) {
         clearInterval(timer);
-        return
+        return;
         }
 
         }, 1000);
       
 }
 
-function questionSelector() {
-    
-    var randCHOICE = Math.floor(Math.random() * 4 );
 
-    if (questionARRAY[0] == randCHOICE) {
+
+function questionSelector() {
+    var correctAnswer = answerB;
+    var correctAnswer2 = answerA;
+    var correctAnswer3 = answerC;
+    var correctAnswer4 = answerC;
+
+    if (questionARRAY[progress] == 0) {
         console.log ("Question 1 has been chosen");
+     
+        
+
         questionDisplay.textContent = "How does a for loop behave in contrast to a while loop";
         answerA.textContent = "A for loop uses a single condition to justify it's loop.";
         answerB.textContent = "A for loop specifies a variable, a conditional comparison, and a additive loop";
         answerC.textContent = "A for loop cannot branch or have alternate conditions.";
-        answerA.addEventListener("click", incorrectUP)
-        answerB.addEventListener("click", correctUP);
-        answerC.addEventListener("click", incorrectUP)
-    } else if (questionARRAY[1] == randCHOICE | correct == 1){
+
+        
+        answerA.addEventListener("click", incorrectUp);
+        correctAnswer.addEventListener("click", correctUp);
+        answerC.addEventListener("click", incorrectUp); 
+
+      
+
+
+        
+        
+
+    } else if (questionARRAY[progress] === 1 ){  
         console.log ("Question 2 has been chosen");
+     
+        
         questionDisplay.textContent = "Why use local storage instead of a variable defined in a script?";
-        answerA.textContent = "The value can be refereced in later iterations of the same browser.";
-        answerB.textContent = "The value can be referenced in later instances of any browser.";
-        answerC.textContent = "The value can be stored as a number type, and be retrieved as a string.";
-        answerA.addEventListener("click", correctUP);
-        answerB.addEventListener("click", incorrectUP)
-        answerC.addEventListener("click", incorrectUP)
-    } else if (questionARRAY[2] == randCHOICE | correct == 2){
+         answerA.textContent = "The value can be refereced in later iterations of the same browser.";
+         answerB.textContent = "The value can be referenced in later instances of any browser.";
+         answerC.textContent = "The value can be stored as a number type, and be retrieved as a string.";
+        
+        
+        
+        correctAnswer2.addEventListener("click", correctUp);
+        answerB.addEventListener("click", incorrectUp);
+        answerC.addEventListener("click", incorrectUp);
+
+        
+
+    } else if (questionARRAY[progress] === 2 ){
+
         console.log ("Question 3 has been chose");
         questionDisplay.textContent = "this is question 3";
         answerA.textContent = "The value returned is for question 3.";
         answerB.textContent = "The value returned is for question 3.";
         answerC.textContent = "The value returned is for question 3";
-        answerA.addEventListener("click", incorrectUP)
-        answerB.addEventListener("click", incorrectUP)
-        answerC.addEventListener("click", correctUP);
-    } else if (questionARRAY[3] == randCHOICE | correct == 3) {
+
+        
+
+
+        answerA.addEventListener("click", incorrectUp);
+        answerB.addEventListener("click", incorrectUp);
+        correctAnswer3.addEventListener("click", correctUp);
+
+        
+    } else if (questionARRAY[progress] === 3) {
         console.log ("Question 4 has been chosen");
         questionDisplay.textContent = "Which of the below answers is correctly syntaxed";
         answerA.textContent = "var htmlSelect = querySelector('.class');";
         answerB.textContent = "if ( i = 0, i < numericalValue, i++ ) { }";
         answerC.textContent = "localStorage.setItem( value , value)";
-        answerA.addEventListener("click", incorrectUP);
-        answerB.addEventListener("click", incorrectUP);
-        answerC.addEventListener("click", correctUP);
-    } else if (timeHost <= 0) {
+
+        
+
+        answerA.addEventListener("click", incorrectUp);
+        answerB.addEventListener("click", incorrectUp);
+        correctAnswer4.addEventListener("click", correctUp);
+
+      
+     
+    } else if (timeHost <= 0 || correct === 4) {
+
+       
+        timeHost == 0;
+        answerA.textContent = "";
+        answerB.textContent = "";
+        answerC.textContent = "";
+        finishStatement();
         return;
+    }
+
+    
     }
 
 
@@ -103,7 +174,7 @@ function questionSelector() {
     }
 
 */
-}
+
 
 /*
 function answerResponse() { 
@@ -123,16 +194,30 @@ function answerResponse() {
 }
 */
 
+function finishStatement() {
+    var name = prompt("Enter your name or initials");
+    var scoreSubmit = prompt("Enter your score");
+    localStorage.setItem("username", JSON.stringify(name));
+    localStorage.setItem("highscore", JSON.stringify((scoreSubmit));
+    var rosterSubmit = document.getElementById("#roster");
+    var usernameSubmit = JSON.parse(localStorage.getItem("username"));
+    var scoreSubmit2 = JSON.parse(localStorage.getItem("highscore"));
+    rosterSubmit.textContent = usernameSubmit + ", " + scoreSubmit2;
+
+    
+}
+
+
 function start() {
     clockDown();
-
-  
+    
     
     questionSelector();
-    if (timeHost <= 0) {
-        timeHost == 0;
-        return timeHost;
-    }
+  
+
+
+    
+    
 }
 
 buttonListen.addEventListener("click", start);
