2f2ed976b753ba90cdcea549c7733e72f8c3a57d
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-05-31 00:03:55 -0700

    A desperate edit, not really sure how to even get the functionality right.

--- Assets/quiz-script.js
+++ Assets/quiz-script.js
@@ -3,54 +3,50 @@ var buttonListen = document.querySelector("#quiz-start");
 var questionDisplay = document.querySelector("#question");
 var incorrectDisplay = document.querySelector("#incorrectScore");
 var correctDisplay = document.querySelector("#correctScore");
+
 var answerA = document.querySelector("#choiceA");
 var answerB = document.querySelector("#choiceB");
 var answerC = document.querySelector("#choiceC");
 
+var total = 0;
+
+
+
 var questionARRAY = [ 0 , 1 , 2 , 3];
-var timeHost = 60;
+var timeHost = 100;
 
 var progress = 0;
 var correct = 0;
-var incorrect = 0;
+var incorrect = total - correct;
 
 
-function grading() {
-    if (correct == 1) {
-        correct == 0;
-        questionSelector();
-        return correct;
-    } else {
-        incorrect++;
-        timeHost-5;
-        questionSelector();
-        return timeHost;
-    }
+function numberOFCLICKS() {
+ total++;
+ console.log("This is the number of clicks:" + total);
+ timeHost = timeHost - incorrect;
+ return total;
 }
 
 
 function correctUp() {
-    correct++;
-    progress++;
-    questionSelector();
     
+    correct++;
+    questionSelector();
     return;
 }
 
-function incorrectUp() {
-    incorrect++;
-    timeHost=timeHost-5;
-    
-    
-    return;
-}
+
+
+
 
 
 
 function clockDown() {
+
     
     var timer = setInterval(function() {
-        incorrectDisplay.textContent = "Incorrect answers: " + incorrect;
+
+        incorrectDisplay.textContent = "Total number of clicks:" + total;
         correctDisplay.textContent = "Correct answers: " + correct;
 
         timeHost--;
@@ -70,79 +66,63 @@ function clockDown() {
 
 
 function questionSelector() {
-    var correctAnswer = answerB;
-    var correctAnswer2 = answerA;
-    var correctAnswer3 = answerC;
-    var correctAnswer4 = answerC;
+   
+    
+    
+    answerA.addEventListener("click" , numberOFCLICKS);
+        answerB.addEventListener("click" , numberOFCLICKS);
+        answerC.addEventListener("click" , numberOFCLICKS);
+   
+    
 
-    if (questionARRAY[progress] == 0) {
+    if (correct === 0) {
         console.log ("Question 1 has been chosen");
-     
+
+        
+        var correctAnswer1 = answerB;
+
+
         
 
         questionDisplay.textContent = "How does a for loop behave in contrast to a while loop";
         answerA.textContent = "A for loop uses a single condition to justify it's loop.";
-        answerB.textContent = "A for loop specifies a variable, a conditional comparison, and a additive loop";
+        correctAnswer1.textContent = "A for loop specifies a variable, a conditional comparison, and a additive loop";
         answerC.textContent = "A for loop cannot branch or have alternate conditions.";
 
-        
-        answerA.addEventListener("click", incorrectUp);
-        correctAnswer.addEventListener("click", correctUp);
-        answerC.addEventListener("click", incorrectUp); 
+        correctAnswer1.addEventListener("click", correctUp);
 
-      
-
-
-        
-        
-
-    } else if (questionARRAY[progress] === 1 ){  
+    } else if (correct === 1 ){  
         console.log ("Question 2 has been chosen");
+        var correctAnswer2 = answerA;
      
-        
+     
+        correctAnswer2.addEventListener("click", correctUp);
         questionDisplay.textContent = "Why use local storage instead of a variable defined in a script?";
-         answerA.textContent = "The value can be refereced in later iterations of the same browser.";
+         correctAnswer2.textContent = "The value can be refereced in later iterations of the same browser.";
          answerB.textContent = "The value can be referenced in later instances of any browser.";
          answerC.textContent = "The value can be stored as a number type, and be retrieved as a string.";
-        
-        
-        
-        correctAnswer2.addEventListener("click", correctUp);
-        answerB.addEventListener("click", incorrectUp);
-        answerC.addEventListener("click", incorrectUp);
 
-        
-
-    } else if (questionARRAY[progress] === 2 ){
-
+    } else if (correct === 2 ){
         console.log ("Question 3 has been chose");
+        var correctAnswer3 = answerC;
+
+        correctAnswer3.addEventListener("click", correctUp);
         questionDisplay.textContent = "this is question 3";
         answerA.textContent = "The value returned is for question 3.";
         answerB.textContent = "The value returned is for question 3.";
-        answerC.textContent = "The value returned is for question 3";
+        correctAnswer3.textContent = "The value returned is for question 3";
 
         
-
-
-        answerA.addEventListener("click", incorrectUp);
-        answerB.addEventListener("click", incorrectUp);
-        correctAnswer3.addEventListener("click", correctUp);
-
-        
-    } else if (questionARRAY[progress] === 3) {
+    } else if (correct === 3) {
+        var correctAnswer4 = answerC;
+       
+        correctAnswer4.addEventListener("click", correctUp);
         console.log ("Question 4 has been chosen");
         questionDisplay.textContent = "Which of the below answers is correctly syntaxed";
         answerA.textContent = "var htmlSelect = querySelector('.class');";
         answerB.textContent = "if ( i = 0, i < numericalValue, i++ ) { }";
-        answerC.textContent = "localStorage.setItem( value , value)";
+        correctAnswer4.textContent = "localStorage.setItem( value , value)";
 
-        
-
-        answerA.addEventListener("click", incorrectUp);
-        answerB.addEventListener("click", incorrectUp);
-        correctAnswer4.addEventListener("click", correctUp);
-
-      
      
     } else if (timeHost <= 0 || correct === 4) {
 
@@ -154,9 +134,19 @@ function questionSelector() {
         finishStatement();
         return;
     }
+        
+        
+        
+        
+        
 
+        
+
+       
+}
     
-    }
+    
+
 
 
   /*  if (questionARRAY[0] == randCHOICE) {
@@ -176,7 +166,7 @@ function questionSelector() {
 */
 
 
-/*
+
 function answerResponse() { 
 
     
@@ -192,17 +182,18 @@ function answerResponse() {
     
 
 }
-*/
+
 
 function finishStatement() {
     var name = prompt("Enter your name or initials");
     var scoreSubmit = prompt("Enter your score");
     localStorage.setItem("username", JSON.stringify(name));
-    localStorage.setItem("highscore", JSON.stringify((scoreSubmit));
+    localStorage.setItem("highscore", JSON.stringify(scoreSubmit));
     var rosterSubmit = document.getElementById("#roster");
     var usernameSubmit = JSON.parse(localStorage.getItem("username"));
     var scoreSubmit2 = JSON.parse(localStorage.getItem("highscore"));
-    rosterSubmit.textContent = usernameSubmit + ", " + scoreSubmit2;
+    var stringify = scoreSubmit2 + usernameSubmit;
+    rosterSubmit.Value = stringify;
 
     
 }
